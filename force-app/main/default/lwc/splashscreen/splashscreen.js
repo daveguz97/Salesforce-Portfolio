c20a0c9b4918a068c2f4e1e9ffac2c589638a964
David Guzman <davidguzmaninc@gmail.com>
2023-12-16 01:46:38 -0500

    Created a splash screen

---
+++ force-app/main/default/lwc/splashscreen/splashscreen.js
@@ -0,0 +1,13 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Splashscreen extends LightningElement {
+	showSplashScreen = true;
+
+	// connectedCallback() {
+	// 	this.setSplashScreen();
+	// }
+	// setSplashScreen() {
+	// 	this.template.querySelector('.border').style = `animation: spinAndFade 1s ease-in-out forwards;`;
+	// }
+	
+}
