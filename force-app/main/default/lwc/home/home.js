d03398cba3eb240e9337dc1b8cac88075c2b1030
David Guzman <davidguzmaninc@gmail.com>
2023-12-18 19:01:36 -0500

    Retrieved everything in org

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -1,14 +1,12 @@
 import { LightningElement } from "lwc";
 import meImg from "@salesforce/resourceUrl/me";
-import homeBackground from "@salesforce/resourceUrl/HomeBackgroundImage";
 
 export default class Home extends LightningElement {
     get img() {
         return meImg;
     }
 
-    // TODO Give Credit to Image by <a href="https://www.freepik.com/free-vector/gradient-mountain-landscape_20008477.htm#query=hiking%20background&position=6&from_view=keyword&track=ais&uuid=dbefd296-07d5-4e33-adda-d5e16fba9c4f">Freepik</a>
-    get backgroundImg() {
-        return `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${homeBackground}); background-size: cover; background-position: center; background-repeat: no-repeat; background-attachment: fixed; overflow: hidden;`;
+    get backgroundColor() {
+        return `background-color: #042B44; background-size: cover; background-position: center;overflow: hidden;`;
     }
 }
