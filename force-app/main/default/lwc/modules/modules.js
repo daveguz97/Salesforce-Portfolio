1ce9edbf409cfb025ba6b3c138bfd42ea860115c
David Guzman <davidguzmaninc@gmail.com>
2023-12-19 22:47:43 -0500

    Added modules

--- force-app/main/default/lwc/modules/modules.js
+++ force-app/main/default/lwc/modules/modules.js
@@ -1,14 +1,38 @@
 import { LightningElement, wire } from "lwc";
 import getModules from "@salesforce/apex/badgesController.getModules";
 
-const COLUMNS = [
-    { label: "Badge Icon", fieldName: "trailheadapp__Badge_Icon__c" },
-    { label: "Name", fieldName: "trailheadapp__Badge__r.Name" }
-];
 export default class Modules extends LightningElement {
+    modules;
     error;
-    columns = COLUMNS;
 
     @wire(getModules)
-	modules
+    wiredModules({ error, data }) {
+        try {
+            if (data) {
+                this.modules = data;
+                this.error = undefined;
+                this.renderIcons();
+            } else {
+                this.error = error;
+            }
+        } catch (err) {
+            console.log(`Error: ${err}`);
+        }
+    }
+
+    renderIcons() {
+        let container = this.template.querySelector(".container");
+		
+		console.log('module', this.modules)
+		
+        this.modules.forEach((module) => {
+			let group = document.createElement('div');
+			group.classList.add('group');
+			container.appendChild(group);
+			group.innerHTML += module.trailheadapp__Badge_Icon__c;
+			let h4 = document.createElement('small')
+			h4.textContent = module.trailheadapp__Badge__r.Name
+			group.appendChild(h4);
+        });
+    }
 }
