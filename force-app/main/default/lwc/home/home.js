71482a24bdb3a8b1e8a4aade1422ceea7a1de6cb
David Guzman <davidguzmaninc@gmail.com>
2023-12-21 00:14:53 -0500

    Added some styling

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -2,6 +2,7 @@ import { LightningElement } from "lwc";
 import meImg from "@salesforce/resourceUrl/me";
 
 export default class Home extends LightningElement {
+	
     get img() {
         return meImg;
     }
--- force-app/main/default/lwc/modules/modules.js
+++ force-app/main/default/lwc/modules/modules.js
@@ -3,41 +3,104 @@ import getBadges from "@salesforce/apex/badgesController.getBadges";
 
 export default class Modules extends LightningElement {
     modules;
+    projects;
+    superbadges;
     error;
-	type = {
-		trailheadapp__Type__c:  'Module'
-	}
+    moduleType = {
+        type: "Module"
+    };
 
-    @wire(getBadges, {bindParams: '$type'})
+    projectType = {
+        type: "Project"
+    };
+
+    superbadgeType = {
+        type: "Superbadge"
+    };
+
+    @wire(getBadges, { bindParams: "$moduleType" })
     wiredModules({ error, data }) {
         try {
             if (data) {
                 this.modules = data;
                 this.error = undefined;
-                this.renderIcons();
-				console.log(this.modules)
+                this.renderIconsAndText(this.modules);
+                console.log(this.modules);
             } else {
                 this.error = error;
-				console.log(this.error)
+                console.log(this.error);
             }
         } catch (err) {
             console.log(`Error: ${err}`);
         }
     }
 
-    renderIcons() {
-        let container = this.template.querySelector(".container");
-		
-		console.log('module', this.modules)
-		
-        this.modules.forEach((module) => {
-			let group = document.createElement('div');
-			group.classList.add('group');
-			container.appendChild(group);
-			group.innerHTML += module.trailheadapp__Badge_Icon__c;
-			let h4 = document.createElement('small')
-			h4.textContent = module.trailheadapp__Badge__r.Name
-			group.appendChild(h4);
+    @wire(getBadges, { bindParams: "$projectType" })
+    wiredProjects({ error, data }) {
+        try {
+            if (data) {
+                this.projects = data;
+                this.error = undefined;
+                this.renderIconsAndText(this.projects);
+                console.log(this.projects);
+            } else {
+                this.error = error;
+                console.log(this.error);
+            }
+        } catch (err) {
+            console.log(`Error: ${err}`);
+        }
+    }
+
+    @wire(getBadges, { bindParams: "$superbadgeType" })
+    wiredSuperbadges({ error, data }) {
+        try {
+            if (data) {
+                this.superbadges = data;
+                this.error = undefined;
+                this.renderIconsAndText(this.superbadges);
+                console.log(this.superbadges);
+            } else {
+                this.error = error;
+                console.log(this.error);
+            }
+        } catch (err) {
+            console.log(`Error: ${err}`);
+        }
+    }
+
+    renderIconsAndText(badgeData) {
+        let modulesContainer =
+            this.template.querySelector(".modules-container");
+        let projectContainer = this.template.querySelector(
+            ".projects-container"
+        );
+        let superBadgeContainer = this.template.querySelector(
+            ".superbadges-container"
+        );
+        badgeData.forEach((badge) => {
+            let group = document.createElement("div");
+            group.classList.add("group");
+            let type = badge.trailheadapp__Badge__r.trailheadapp__Type__c;
+            let icon = badge.trailheadapp__Badge_Icon__c;
+            let title = badge.trailheadapp__Badge__r.Name;
+            let smallText = document.createElement("small");
+            group.innerHTML += icon;
+            smallText.textContent = title;
+            group.appendChild(smallText);
+            switch (type) {
+                case "Module":
+                    modulesContainer.appendChild(group);
+                    break;
+                case "Project":
+                    projectContainer.appendChild(group);
+                    break;
+                case "Superbadge":
+                    superBadgeContainer.appendChild(group);
+                    break;
+                default:
+                    console.log("Type is invalid");
+            }
         });
     }
 }
