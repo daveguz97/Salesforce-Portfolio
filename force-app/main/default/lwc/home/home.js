954110a21bcb0e6b343b064c38b4e9b38b8e4521
David Guzman <davidguzmaninc@gmail.com>
2023-12-24 15:21:49 -0500

    Added the first section of the about me

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -6,8 +6,4 @@ export default class Home extends LightningElement {
     get img() {
         return meImg;
     }
-
-    get backgroundColor() {
-        return `background-color: #042B44; background-size: cover; background-position: center;overflow: hidden;`;
-    }
 }
