84d8cfe462783b6fc144f1ae2b5f9b9387f32987
David Guzman <davidguzmaninc@gmail.com>
2023-12-26 20:25:56 -0500

    Added Services to a new component

--- force-app/main/default/lwc/about/about.js
+++ force-app/main/default/lwc/about/about.js
@@ -1,26 +1,4 @@
 import { LightningElement } from "lwc";
-
-let id = 0;
-const ICONS = [
-    {
-        id: id++,
-        name: "utility:apex",
-        alternativeText: "Apex Development",
-        text: "Apex Development"
-    },
-    {
-        id: id++,
-        name: "utility:insert_tag_field",
-        alternativeText: "Lightning Components",
-        text: "Lightning Components"
-    },
-    {
-        id: id++,
-        name: "utility:setup",
-        alternativeText: "Salesforce Administration",
-        text: "Salesforce Administration"
-    }
-];
 export default class About extends LightningElement {
-    icons = ICONS;
+    
 }
